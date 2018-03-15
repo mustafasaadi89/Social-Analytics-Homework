58de48ef69d2b996899c6a2aeef20e034feefd9c
Mustafa <mustafa.saadi89@gmail.com>
2018-03-14 19:42:53 -0700

    adding homework files

---
+++ TwitterConfig.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "CNxglqCtUkgfIgZBUe5g7yIOn"
+consumer_secret = "4wcjWueKMzIMobugMQW50nsKs3dD87NIAbQr9482euCrd3JE53"
+access_token = "858735119514845184-IIsQs9jsd3JeWDHKRxpp6CvCq8NoolJ"
+access_token_secret = "l4Q2JkvIk0ub7wxzrHg08Z1K1sJxgiH6WPYi1uYglL8vH"
